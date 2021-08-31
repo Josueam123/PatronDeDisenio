96e4b2f6de7a166cc647a14789ba93c0d2bb3cc0
GEANPIERRE JOSUE AGUIRRE MENDOZA <e1314343920@live.uleam.edu.ec>
2021-08-31 13:53:10 -0500

    Agregar archivos de proyecto.

---
+++ EmailManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronDeDisenio
+{
+    class EmailManager
+    {
+        //METODOS GETTER Y SETTER DE TODA LA VIDA
+        public string cliente { get; set; }
+        public string desde { get; set; }
+        public string para { get; set; }
+        public string asunto { get; set; }
+        public string cuerpo { get; set; }
+
+
+        //METODOS QUE NOS AYUDAN A EVITAR REDUNDANCIA
+        public EmailManager Cliente (string cliente)
+        {
+            this.cliente = cliente;
+            return this;
+        }
+
+        public EmailManager Desde(string desde)
+        {
+            this.desde = desde;
+            return this;
+        }
+
+        public EmailManager Para (string para)
+        {
+            this.para = para;
+            return this;
+        }
+
+        public EmailManager Asunto (string asunto)
+        {
+            this.asunto = asunto;
+            return this;
+        }
+
+        public EmailManager Cuerpo (string cuerpo)
+        {
+            this.cuerpo = cuerpo;
+            return this;
+        }
+
+        public void enviar()
+        {
+            Console.WriteLine("El Email ha sido enviado correctamente");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronDeDisenio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciar la clase EmailManager
+            EmailManager em = new EmailManager();
+
+            //redundancia al utilizar los metodos getter y setter tradicionales
+            // observamos redundancia ya que ya que "em" se repite un importante numero de veces y podemos mejorarlo
+            //em.cliente = "Roberto Perez";
+            //em.desde ="anohter@hotmail.com";
+            //em.para = "your-mail@gmail.com";
+            //em.asunto = "Saludo";
+            //em.cuerpo = "Gracias por sus comentarios";
+
+
+
+
+            // PATRON DE DISEÑO CASCADA
+            // nueva forma de instanciar
+            EmailManager nuevoEmail = new EmailManager()
+                .Cliente("Roberto Perez")
+                .Desde("another@hotmail.com")
+                .Para("your-nauk@gmail.com")
+                .Asunto("Saludo")
+                .Cuerpo("Gracias por sus comentarios");
+
+            nuevoEmail.enviar();
+
+            // como resultado se logra eliminar codigo duplicado logrando mas legibilidad y simplicidad de uso
+        }
+    }
+}
